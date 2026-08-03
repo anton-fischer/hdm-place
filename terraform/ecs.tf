3bc24c1eed421bad7db83aee91727c869b3a9b5f
Anton Fischer <anton.fischer.18@gmail.com>
2026-08-03 15:13:50 +0200

    replace ec2 with ecs setup

---
+++ terraform/ecs.tf
@@ -0,0 +1,124 @@
+resource "aws_ecs_cluster" "hdm_place" {
+  name = "hdm-place"
+}
+
+resource "aws_cloudwatch_log_group" "backend" {
+  name              = "/ecs/hdm-place-backend"
+  retention_in_days = 14
+}
+
+resource "aws_iam_role" "ecs_task_execution" {
+  name = "hdm-place-ecs-task-execution"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = { Service = "ecs-tasks.amazonaws.com" }
+        Action    = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution" {
+  role       = aws_iam_role.ecs_task_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_ecs_task_definition" "backend" {
+  family                   = "hdm-place-backend"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "backend"
+      image     = "${aws_ecr_repository.backend.repository_url}:latest"
+      essential = true
+
+      portMappings = [
+        {
+          containerPort = 3001
+          protocol      = "tcp"
+        }
+      ]
+
+      environment = [
+        {
+          name  = "DATABASE_URL"
+          value = "postgresql://hdm_place:${var.DB_PASSWORD}@${aws_db_instance.db.endpoint}/hdm_place?schema=public"
+        }
+      ]
+
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = aws_cloudwatch_log_group.backend.name
+          "awslogs-region"        = "eu-north-1"
+          "awslogs-stream-prefix" = "backend"
+        }
+      }
+    }
+  ])
+}
+
+resource "aws_lb" "hdm_place" {
+  name               = "hdm-place-alb"
+  load_balancer_type = "application"
+  internal           = false
+  security_groups    = [aws_security_group.sg_alb.id]
+  subnets            = [aws_subnet.public_subnet_ec2.id, aws_subnet.public_subnet_b.id]
+
+  # Keep /ws connections open, ALB is dropping them at 60s per default
+  idle_timeout = 300
+}
+
+resource "aws_lb_target_group" "backend" {
+  name        = "hdm-place-backend-tg"
+  port        = 3001
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.vpc.id
+  target_type = "ip"
+
+  health_check {
+    path = "/health"
+  }
+}
+
+resource "aws_lb_listener" "backend" {
+  load_balancer_arn = aws_lb.hdm_place.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.backend.arn
+  }
+}
+
+resource "aws_ecs_service" "backend" {
+  name            = "backend"
+  cluster         = aws_ecs_cluster.hdm_place.id
+  task_definition = aws_ecs_task_definition.backend.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = [aws_subnet.public_subnet_ec2.id, aws_subnet.public_subnet_b.id]
+    security_groups  = [aws_security_group.sg_ecs_tasks.id]
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.backend.arn
+    container_name   = "backend"
+    container_port   = 3001
+  }
+
+  depends_on = [aws_lb_listener.backend]
+}
