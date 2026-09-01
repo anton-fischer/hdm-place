d10d8d5fcf7ed69bd0bcb22880e12add71e40a99
Coun7ered <erik.bondel@gmx.de>
2026-09-01 12:27:47 +0200

    move test to src

--- backend/src/tests/userCooldown.test.ts
+++ backend/src/tests/userCooldown.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest'
-import { buildApp } from '../src/app.js'
+import { buildApp } from '../app.js'
 
 describe('user cooldown route', () => {
     it('returns cooldown info for given id', async () => {
